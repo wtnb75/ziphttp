5ef75b094515c995101cf570ce17658a0a65f26f
user 123 <wtnb75@gmail.com>
2025-03-07 22:45:28 +0900

    webserver serve .gz

--- main.go
+++ main.go
@@ -67,7 +67,7 @@ func main() {
 		}
 	}
 	if _, err := parser.Parse(); err != nil {
-		if fe, ok := err.(*flags.Error); ok && fe.Type == flags.ErrHelp {
+		if _, ok := err.(*flags.Error); ok {
 			os.Exit(0)
 		}
 		slog.Error("error exit", "error", err)
